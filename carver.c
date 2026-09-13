d5889c8c1b39d1fb08287e158535a42ef3080095
Bogdan Mitrović <bogdanmitrovic17102001@gmail.com>
2026-09-13 14:05:13 +0200

    Add: JPEG carving from header to footer
    Once footer is found, if the header was found before, a file is considered valid and saved.

--- carver.c
+++ carver.c
@@ -4,6 +4,7 @@
 
 const unsigned char HEADER[] = {0xFF, 0xD8, 0xFF};  // JPEG start
 const unsigned char FOOTER[] = {0xFF, 0xD9};        // JPEG end
+long files_carved = 0;
 
 int main (int argc, char *argv[])
 {
@@ -27,19 +28,38 @@ int main (int argc, char *argv[])
     {
         for (size_t i = 0; i < bytes_read - 2; i++)
         {
-            // printf("bytes read is %ld\n", bytes_read);
-            if (memcmp(&buffer[i], HEADER, 2) == 0)
+            if (memcmp(&buffer[i], HEADER, 3) == 0)
             {
                 printf("Found JPEG header at offset: %lu\n", ftell(input_file) - bytes_read + i);
                 header_offset = ftell(input_file) - bytes_read + i;
             }
             else if (memcmp(&buffer[i], FOOTER, 2) == 0)
             {
-                printf("Found JPEG footer at offset: %lu\n", ftell(input_file) - bytes_read + i);
+                int footer_offset = ftell(input_file) - bytes_read + i;
+                printf("Found JPEG footer at offset: %lu\n", footer_offset);
                 if (header_offset != -1)
                 {
-                    printf("Carving JPEG from offset %ld to %lu\n", header_offset, ftell(input_file) - bytes_read + i + 2);
+                    printf("Carving JPEG from offset %ld to %lu\n", header_offset, footer_offset + 2);
+                    char output_filename[256];
+                    snprintf(output_filename, sizeof(output_filename), "%s/carved_%ld.jpg", argv[2], files_carved++);
+                    FILE* output_file = fopen(output_filename, "wb");
+
+                    fseeko(input_file, header_offset, SEEK_SET);
                     header_offset = -1;
+                    
+                    unsigned long file_bytes = 0;
+                    while ( ftell(input_file) != footer_offset + 2)
+                    {
+                        int b = fgetc(input_file);
+                        if (b == EOF)
+                        {
+                            break;
+                        }
+                        fputc(b, output_file);
+                        file_bytes++;
+                    }
+                    fclose(output_file);
+                    printf("Carved %lu bytes to %s\n", file_bytes, output_filename);
                 }
             }
         }
